d87a1365ff25ab906bd1dfbd96ef474ed02d1879
pravin025 <44036829+pravin025@users.noreply.github.com>
2019-07-22 16:39:48 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
